06dd845007ee39196a1e1a126c0f3b294e0dc812
Carl Darwin B. Cortes <100175731+CarlDarwinCortes@users.noreply.github.com>
2022-03-03 15:53:15 +0800

    Add files via upload

---
+++ Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int minuend, subtrahend, difference;
+    minuend = 87;
+    subtrahend = 15;
+    difference = minuend - subtrahend;
+
+    printf("The difference between subtracting %d from %d is %d.", subtrahend, minuend, difference);
+
+    return 0;
+
+}
+
